5453ca8a2c4111736dbdbd25b2cdc916389811ed
Aneta Maciejczak <aneta.maciejczak@gmail.com>
2018-06-22 15:37:39 +0200

    game_3

--- js/game.js
+++ js/game.js
@@ -9,12 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
         zombie.classList.add("zombie");
 
         /*usatwiamy losowąlokalizację*/
-
-
         var position_horizontal = Math.floor(Math.random()*(250-5+1)+5);
         zombie.style.bottom = position_horizontal + "px";
 
-
+        /*ustawiam wilekość*/
         if (position_horizontal > 150) {
             var scaleBig = Math.random()*(0.75-0.2)+0.2;
             zombie.style.transform = "scale("+scaleBig+")";
@@ -23,12 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
             zombie.style.transform = "scale("+scaleSmall+")";
         }
 
-        var time = Math.random()*(12-6)+6;
+        /*ustawiam szybkość poruszania się*/
+        var time = (Math.random()*(12-6+1)+6);
+
         zombie.style.animationDuration = "0.5s,"+time+"s";
-        zombie.style.zIndex = 300- position_horizontal;
+        zombie.style.zIndex = 270 - position_horizontal;
+
 
         board.appendChild(zombie);
 
+        /*ustaiwam usuwanie po kliknięciu*/
         zombie.addEventListener("click", function () {
             this.remove();
 
